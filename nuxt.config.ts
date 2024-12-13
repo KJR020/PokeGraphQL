275a5486c74f5b7c5dab949abebec43ed8af41a2
KJR020 <johnjiro1114@gmail.com>
2024-12-13 23:19:52 +0900

    Vuetifyプラグインをインポートし、Nuxt設定を更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import vuetify from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -5,9 +7,4 @@ export default defineNuxtConfig({
   build : {
     transpile: ['vuetify']
   },
-  vite: [
-    plugins: [
-      vuetify()
-    ]
-  ]
 });
