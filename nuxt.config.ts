2ec1f4e3559d448f8ac23b1a6c8c73abe65268d8
KJR020 <johnjiro1114@gmail.com>
2024-12-14 12:07:59 +0900

    Vuetifyプラグインを追加し、スタイルシートをインポート

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,10 @@ import vuetify from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  plugins: ["@/plugins/vuetify"],
+  css: ["vuetify/styles"],
   devtools: { enabled: true },
-  build : {
-    transpile: ['vuetify']
+  build: {
+    transpile: ["vuetify"],
   },
 });
