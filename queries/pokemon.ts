17d3a91b5dec2b3e03cbe7cdedabeaa8d789a160
KJR020 <johnjiro1114@gmail.com>
2024-12-15 21:49:09 +0900

    ポケモンのクエリに攻撃と進化情報を追加

--- queries/pokemon.ts
+++ queries/pokemon.ts
@@ -24,9 +24,33 @@ const GET_POKEMON = gql`
   query GetPokemon($name: String!) {
     pokemon(name: $name) {
       id
+      number
       name
       types
       image
+      attacks {
+        special {
+          name
+          type
+          damage
+        }
+      }
+      evolutions {
+        id
+        number
+        name
+        weight {
+          minimum
+          maximum
+        }
+        attacks {
+          fast {
+            name
+            type
+            damage
+          }
+        }
+      }
     }
   }
 `;
