d842689a70b04de6d63de4aef966238ac9fc9412
KJR020 <johnjiro1114@gmail.com>
2024-12-24 21:38:00 +0900

    ♻️ AppBar.vueのリンク定義を修正し、ボタンに正しいプロパティを追加

--- components/organisms/AppBar.vue
+++ components/organisms/AppBar.vue
@@ -2,10 +2,10 @@
 import { useLocale } from 'vuetify';
 
 
-const routes = [
+const links = [
   { name: 'Home', path: '/' },
   { name: 'List', path: '/list' },
-  { name: 'Favorite', path: '/favorite' },
+  // { name: 'Favorite', path: '/favorite' },
 ]
 
 useLocale()
@@ -16,7 +16,7 @@ useLocale()
   <v-app-bar flat>
     <v-container class="mx-auto d-flex align-center justify-center">
       <v-avatar class="me-4 " color="grey-darken-1" size="32"></v-avatar>
-      <v-btn v-for="link in links" :key="link" :text="link" variant="text"></v-btn>
+      <v-btn v-for="link in links" :key="link.name" :text="link.name" :to="link.path" variant="text"></v-btn>
       <v-spacer></v-spacer>
       <v-responsive max-width="160">
         <v-text-field density="compact" label="Search" rounded="lg" variant="solo-filled" flat hide-details
