ebf53a510e450a0dc9ce63512244faa3339f46d6
KJR020 <johnjiro1114@gmail.com>
2025-01-30 00:01:24 +0900

    ✨ ポケモンタイプアイコンをリストコンポーネントと選択メニューに統合

--- components/molecules/List.vue
+++ components/molecules/List.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+// Array of Pokémon types to demonstrate
+const types = ['fire', 'water', 'grass', 'electric', 'psychic']
 </script>
 
 <template>
   <v-list rounded="lg">
-    <v-list-item v-for="n in 5" :key="n" :title="`List Item ${n}`" link></v-list-item>
+    <v-list-item v-for="type in types" :key="type" link>
+      <template v-slot:prepend>
+        <TypeIcon :type="type" size="24px" />
+      </template>
+      <v-list-item-title>{{ type.charAt(0).toUpperCase() + type.slice(1) }} Type</v-list-item-title>
+    </v-list-item>
     <v-divider class="my-2"></v-divider>
-    <v-list-item color="greyj-lighten-4" title="Refresh" link></v-list-item>
+    <v-list-item color="grey-lighten-4" title="Refresh" link></v-list-item>
   </v-list>
 </template>
