44685ad5ff18e6c4ac33a0417933f0f00d08d65e
KJR020 <johnjiro1114@gmail.com>
2025-02-23 20:44:28 +0900

    ✨ PiniaをNuxtモジュールに追加し、依存関係を更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import vuetify from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   plugins: ["@/plugins/vuetify"],
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", "@pinia/nuxt"],
   css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
   devtools: { enabled: true },
   build: {
